a2d49ef0486e8ec5bd404348198efbd960bd6089
Anshad UK <anshadu@gmail.com>
2024-12-07 16:45:20 +0530

    updated

--- ATM.py
+++ ATM.py
@@ -28,3 +28,58 @@ def login(pin):
 
 def show_user_balance(balance):
     print(f"Your balance is ${balance}")
+
+def deposit():
+    amount = float(input("Input the amount you want to deposit: ₹"))
+    if amount <= 0:
+        print("No, you can't deposit an insignificant amount.")
+        return 0
+    return amount
+
+def withdraw_money(balance):
+    amount = float(input("Enter the amount of money you want to withdraw: ₹"))
+    if amount > balance:
+        print("You don't have sufficient funds for this.")
+        return 0
+    elif amount <= 0:
+        print("You can only withdraw more than ₹0.")
+        return 0
+    return amount
+
+def main():
+    print("Welcome to SBI Bank!")
+    pin = create_pin()
+
+    if not login(pin):
+        return
+    
+    balance = 0
+    customer_engagement_is_running = True
+
+    while customer_engagement_is_running:
+        print("*************************************")
+        print("1. Show Balance")
+        print("2. Deposit")
+        print("3. Withdraw")
+        print("4. Exit")
+        print("*************************************")
+
+        choice = input("Please enter your choice between 1 and 4: ")
+
+        if choice == "1":
+            show_user_balance(balance)
+        elif choice == "2":
+            balance += deposit()
+            print("Deposit successful.")
+        elif choice == "3":
+            balance -= withdraw_money(balance)
+            print("withdrawal successful.")
+        elif choice == "4":
+            customer_engagement_is_running = False
+        else:
+            print("Invalid choice,please pick between 1 and 4.")
+    
+    print("Thank you for banking with us!")
+
+    if __name__ == "__main__":
+        main()
